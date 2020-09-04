7d733c0d82d4e9903bb10e4da06799e1e3f5e459
ergi98 <edervishaj98@gmail.com>
2020-09-04 17:01:56 +0200

    Added icons to the navbar. Fixed a issue where the acive navlink would not update. Did some changes to the style of the active link

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import './NavBar.scss'
 
+import { NavLink } from 'react-router-dom'
+
 // Bootstrap
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
+// Icons 
+import { ImHome } from 'react-icons/im'
+import { FaWallet } from 'react-icons/fa' 
+import { BsFillBarChartFill } from 'react-icons/bs'
+
 class NavBar extends React.Component {
+
     render () {
         return (
             <Navbar fixed="bottom" expand bg="dark" variant="dark" sticky="bottom" className="my_nav">
                 <Nav fill defaultActiveKey="/home">
                     <Nav.Item>
-                        <Nav.Link href="/portfolios">Portfolios</Nav.Link>
+                        <NavLink to="/portfolios" className="link" activeClassName="selected">
+                            <div className="link-desc"><FaWallet className="icon"/> Portfolios </div>
+                        </NavLink>
                     </Nav.Item>
                     <Nav.Item> 
-                        <Nav.Link href="/home">Home</Nav.Link>
+                        <NavLink to="/home"  className="link" activeClassName="selected">
+                            <div className="link-desc"><ImHome className="icon"/> Home</div>
+                        </NavLink>
                     </Nav.Item>
                     <Nav.Item> 
-                        <Nav.Link href="/statistics">Statistics</Nav.Link>
+                        <NavLink to="/statistics"  className="link" activeClassName="selected">
+                            <div className="link-desc"><BsFillBarChartFill className="icon"/> Statistics </div>
+                        </NavLink>
                     </Nav.Item>
                 </Nav>
             </Navbar>
