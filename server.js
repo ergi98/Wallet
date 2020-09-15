a5d94974ddf7080a1eea2d1b95c59819d7053a55
ergi98 <edervishaj98@gmail.com>
2020-09-15 22:46:08 +0200

    Structure changes

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use('/transactions', transactionRouter)
 
 // Serve our static assets if in production
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(__dirname, 'frontend', 'build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
